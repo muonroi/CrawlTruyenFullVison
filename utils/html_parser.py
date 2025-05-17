27450f53f0c207a47c305aa77f6ed42769ed6892
muonroi <leanhphi1706@gmail.com>
2025-05-17 13:11:20 +0700

    feat: Add header pattern matching to improve chapter content extraction

--- utils/html_parser.py
+++ utils/html_parser.py
@@ -2,31 +2,31 @@ import re
 from typing import List
 from bs4 import BeautifulSoup
 
-from config.config import  PATTERN_FILE
+from config.config import  HEADER_RE, PATTERN_FILE
 from utils.cleaner import clean_chapter_content
 from utils.io_utils import filter_lines_by_patterns, load_patterns
 
 BLACKLIST_PATTERNS = load_patterns(PATTERN_FILE)
 
 def extract_chapter_content(html: str, patterns: List[re.Pattern]=BLACKLIST_PATTERNS) -> str:
+    from bs4 import BeautifulSoup
     soup = BeautifulSoup(html, "html.parser")
     chapter_div = soup.find("div", id="chapter-c")
     if not chapter_div:
         return ""
     clean_chapter_content(chapter_div)
-    text = chapter_div.get_text(separator="\n")
-    lines = [line.strip() for line in text.splitlines()]
+
+    lines = []
+    for p in chapter_div.find_all("p"): #type: ignore
+        line = p.get_text("", strip=True) 
+        if line:
+            lines.append(line)
+
     cleaned_lines = filter_lines_by_patterns(lines, patterns)
     content = clean_header("\n".join(cleaned_lines)).strip()
     return content
 
-HEADER_PATTERNS = [
-    r"^nguồn:",
-    r"^truyện:",
-    r"^thể loại:",
-    r"^chương:",
-]
-HEADER_RE = re.compile("|".join(HEADER_PATTERNS), re.IGNORECASE)
+
 
 def clean_header(text: str):
     lines = text.splitlines()
